64a35bbe7b7862e746c3ace25800c12525f827b5
francisco-cruz <franaraujosilva2017@gmail.com>
2022-12-24 12:17:40 -0300

    style: editando alguns detalhes

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -65,5 +65,5 @@ module.exports = {
   createTask,
   getById,
   updateOneTask,
-  deleteOneTask
+  deleteOneTask,
 };
--- models/Task.js
+++ models/Task.js
@@ -15,7 +15,7 @@ const taskSchema = new mongoose.Schema({
   },
   edited: {
     type: Boolean,
-  }
+  },
 });
 
 module.exports = mongoose.model("Task", taskSchema);
